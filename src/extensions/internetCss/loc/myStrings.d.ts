f693eb735aa67fe1297e743fb9028cb6ab422543
MounikaJangam <mounikaj404@gmail.com>
2023-03-31 14:42:03 +0530

    OVerridingCss

---
+++ src/extensions/internetCss/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IInternetCssApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'InternetCssApplicationCustomizerStrings' {
+  const strings: IInternetCssApplicationCustomizerStrings;
+  export = strings;
+}
